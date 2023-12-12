442e1fd2989844595cc17cfef472ffb10a043fd0
ianseeingbeing <igillette2006@gmail.com>
2023-12-12 09:34:46 -0500

    set up day2

---
+++ day2/main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <vector>
+
+int part_one();
+int part_two();
+
+int main() {
+
+    std::cout << part_one() << std::endl;
+
+    return 0;
+}
+
+int part_one() {
+
+    return 0;
+}
+
+int part_two() {
+
+    return 0;
+}
